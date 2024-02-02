ba0329373f3687377349e330b9cd088f1d608f48
Shahanaz Karim <shahnaz.karim101@gmail.com>
2024-02-02 20:51:58 +0100

    adding input

--- src/lib/UI/App.svelte
+++ src/lib/UI/App.svelte
@@ -1,5 +1,27 @@
 <script>
     import Header from "./Header.svelte";
+    import MeetupsItem from "./Meetups/MeetupsItem.svelte";
+    const meetups = [
+        {
+            id: "m1",
+            title: "coding bootcamp",
+            subtitle: "Learn to code in 2 hours",
+            description: "Hi Hello Bye",
+            address: 12345,
+            contactEmail: "tarin@gamil.com",
+        },
+        {
+            id: "m2",
+            title: "cook together",
+            subtitle: "Learn to cook in 2 hours",
+            description: "Hi Hello Bye",
+            address: 12345,
+            contactEmail: "tarin@gamil.com",
+        },
+    ];
 </script>
 
 <Header></Header>
+{#each meetups as meetup}
+    <MeetupsItem />
+{/each}
